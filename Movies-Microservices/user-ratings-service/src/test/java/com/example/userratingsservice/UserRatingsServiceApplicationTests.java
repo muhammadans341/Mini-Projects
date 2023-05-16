d1c83b5a36bdcae1243dcdbc80d9a8a9f75bf04c
Muhammad Ans <muhammad.ans@seamless.se>
2023-05-16 23:14:26 +0500

    initail microservices commit

---
+++ Movies-Microservices/user-ratings-service/src/test/java/com/example/userratingsservice/UserRatingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userratingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRatingsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
