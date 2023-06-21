d94a6b51982f7afb63dc767ccb217fd8d6cfb4f4
Muhammad Ans <muhammad.ans@seamless.se>
2023-06-21 19:59:55 +0500

    added movie microservices for POC of spring cloud

--- Movies-Microservices/movie-catalog-service/src/main/java/com/example/moviecatalogservice/service/MovieInfoService.java
+++ Movies-Microservices/movie-catalog-service/src/main/java/com/example/moviecatalogservice/service/MovieInfoService.java
@@ -5,16 +5,20 @@ import com.example.moviecatalogservice.model.Movie;
 import com.example.moviecatalogservice.model.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class MovieInfoService {
+    @Value("${service.url.movie-info}")
+    private String movieInfoUrl;
+
     private final RestTemplate restTemplate;
     @HystrixCommand(fallbackMethod = "getDefaultCatalogItem")
     public CatalogItem getCatalogItem(Rating rating) {
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+ rating.getMovieId(), Movie.class);
+        Movie movie = restTemplate.getForObject(movieInfoUrl+"/movies/"+ rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getTitle(), "desc", rating.getRating());
     }
     public CatalogItem getDefaultCatalogItem(Rating rating) {
--- Movies-Microservices/movie-catalog-service/src/main/java/com/example/moviecatalogservice/service/UserRatingService.java
+++ Movies-Microservices/movie-catalog-service/src/main/java/com/example/moviecatalogservice/service/UserRatingService.java
@@ -3,7 +3,9 @@ package com.example.moviecatalogservice.service;
 import com.example.moviecatalogservice.model.Rating;
 import com.example.moviecatalogservice.model.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,10 +14,21 @@ import java.util.Collections;
 @Service
 @RequiredArgsConstructor
 public class UserRatingService {
+
+    @Value("${service.url.user-rating}")
+    private String userRatingUrl;
+
     private final RestTemplate restTemplate;
-    @HystrixCommand(fallbackMethod = "getDefaultUserRating")
+    @HystrixCommand(
+            fallbackMethod = "getDefaultUserRating",
+            threadPoolKey = "userRatingThreadPool",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize",value = "20"),
+                    @HystrixProperty(name="maxQueueSize",value = "10")
+            }
+    )
     public UserRating getUserRating(String userId) {
-        UserRating userRating = restTemplate.getForObject("http://user-rating-service/ratingdata/users/"+ userId, UserRating.class);
+        UserRating userRating = restTemplate.getForObject(userRatingUrl+"/ratingdata/users/"+ userId, UserRating.class);
         return userRating;
     }
     public UserRating getDefaultUserRating(String userId) {
