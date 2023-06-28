6c0cc3660c6c1b0af7f8f71322fbba72e9e34b84
Muhammad Ans <muhammad.ans@seamless.se>
2023-06-28 17:44:00 +0500

    initial unitesting commit

---
+++ UnitTesting/src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
+class StudentRepositoryTest {
+    @Test
+    void itShouldCheckIfStudentEmailExists(){
+
+    }
+}
---
+++ UnitTesting/src/test/java/com/example/unittesting/UnitTestingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class UnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
