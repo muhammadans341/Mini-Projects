eb48d83fbcc02ffe6eeaab5fb87da5b5e18d058a
Muhammad Ans <muhammad.ans@seamless.se>
2023-07-06 22:20:53 +0500

    added rabbit listener in notification service

--- microservices/notification/src/main/java/org/microservices/notification/NotificationApplication.java
+++ microservices/notification/src/main/java/org/microservices/notification/NotificationApplication.java
@@ -1,13 +1,17 @@
 package org.microservices.notification;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages =
+        {   "org.microservices.notification","org.microservices.amqp"
+        })
 @EnableFeignClients(basePackages = "org.microservices.clients")
 @EnableEurekaClient
+@EnableRabbit
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
--- microservices/notification/src/main/java/org/microservices/notification/rabbitmq/NotificationConsumer.java
+++ microservices/notification/src/main/java/org/microservices/notification/rabbitmq/NotificationConsumer.java
@@ -1,2 +1,24 @@
-package org.microservices.notification.rabbitmq;public class NotificationConsumer {
+package org.microservices.notification.rabbitmq;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.microservices.clients.notification.NotificationRequest;
+import org.microservices.notification.service.NotificationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumeNotification(@Payload NotificationRequest notificationRequest){
+        log.info("Converting back to object");
+
+        log.info("Consuming notification request {}",notificationRequest);
+        //notificationService.sendNotification((NotificationRequest) notificationRequest);
+    }
+
 }
