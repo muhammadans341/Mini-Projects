219e486a105a571b0f7abf0a307d315513ba4d4c
Muhammad Ans <muhammad.ans@seamless.se>
2023-07-06 22:22:32 +0500

    added apigw support

--- microservices/apigw/src/main/java/org/microservices/ApiGWApplication.java
+++ microservices/apigw/src/main/java/org/microservices/ApiGWApplication.java
@@ -1,7 +1,13 @@
 package org.microservices;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGWApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ApiGWApplication.class,args);
     }
 }
