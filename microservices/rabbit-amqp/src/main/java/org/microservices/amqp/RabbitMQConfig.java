c1b8122090d1c52d7fbcab8cfc28a5e6289030ca
Muhammad Ans <muhammad.ans@seamless.se>
2023-07-08 19:57:52 +0500

    Added docker support for all microservices

--- microservices/rabbit-amqp/src/main/java/org/microservices/amqp/RabbitMQConfig.java
+++ microservices/rabbit-amqp/src/main/java/org/microservices/amqp/RabbitMQConfig.java
@@ -7,6 +7,8 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
